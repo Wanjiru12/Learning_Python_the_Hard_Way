2b0e0215ded22fd7d0f1a4c9a4af2ffb90c37bd2
Catherine Wanjiru Mwai <CWMWAI@safaricom.co.ke>
2024-03-11 15:13:45 +0300

    Create week02 work

---
+++ Week02/Inputs.py
@@ -0,0 +1,9 @@
+print('What is you name'),
+name = input()
+print("What is your age"),
+age = input()
+print("What is your weight"),
+weight = input()
+
+intro= "My name is {} I am, {} and my weight is {}"
+print(intro.format(name, age, weight))
